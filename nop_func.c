ddb83a5a47a758551b20babdca40510c4d47f4f6
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-04 14:45:12 -0300

    nop

---
+++ nop_func.c
@@ -0,0 +1,11 @@
+/**
+*nop_func - doesn´t do anything
+*@stack: pointer to top the stack
+*@line_number: number of the line
+*/
+
+void nop_func(stack_t **stack, unsigned int line_number);
+{
+    (void) stack;
+    (void) line_number;
+}
