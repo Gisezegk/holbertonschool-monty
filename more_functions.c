8f039224a7cbed397d753f57467aaef60e84aae6
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-05 19:46:42 -0300

    morefunctionsmonty

--- more_functions.c
+++ more_functions.c
@@ -46,3 +46,28 @@ void swapStack(stack_t **stack, unsigned int line_number)
 	head->n = head->next->n;
 	head->next->n = n;
 }
+
+/**
+* addStack - adds the top two elements of the stack
+*@line_number: line of the number
+*@stack: pointer to top the stack
+*/
+
+void addStack(stack_t **stack, unsigned int line_number);
+{
+	stack_t *f;
+	int a;
+
+	if (!stack || !(*stack) || !(*stack)->next)
+	{
+		printf("L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	f = *stack;
+	a = j->n + j->next->n;
+	f->next->n = a;
+
+	*stack = j->next;
+	free(j);
+}
