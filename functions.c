1e3063aa777d097d46709cc25257ce55936ae39f
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-05 19:46:17 -0300

    functionsmonty

--- functions.c
+++ functions.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 
 /**
-* pallStack - print all the values on the stack 
+* pallStack - print all the values on the stack
 * starting from the top of the stack
 * @stack: pointer to top the stack
 * @line_number: number of the line
@@ -11,7 +11,7 @@ void pallStack (stack_t **stack, unsigned int line_number)
 {
     stack_t *tmp = *stack;
     tmp = *stack;
-    line_number = line_number;
+    (void)line_number;
 
     while (tmp =! NULL)
     {
@@ -22,7 +22,38 @@ void pallStack (stack_t **stack, unsigned int line_number)
 }
 
 /**
-* pint_Stack - print the value top of the stack
+*pushStack - pushes an element to the stack
+*@line_number: number of the line
+*@stack: pointer to top the stack
+*/
+
+void pushStack(stack_t **stack, unsigned int line_number);
+{
+    stack_t *nodo;
+    char *j;
+
+    j = strtok(NULL, DEL);
+    if (j == NULL)
+    {
+        printf("L%u:usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+nodo = malloc(sizeof(stack_t));
+if (nodo == NULL)
+{
+    printf("Error: malloc failed\n");
+    exit(EXIT_FAILURE);
+}
+nodo->n = atoi(j);
+nodo->prev = NULL;
+nodo->next = *stack;
+if (*stack != NULL)
+(*stack)->prev = nodo;
+*stack = nodo;
+}
+
+/**
+* pintStack - print the value top of the stack
 *@stack: pointer to top the stack
 *@line_number: number of the line
 */
@@ -36,7 +67,6 @@ void pintStack(stack_t **stack, unsigned int line_number);
     }
     printf("%d\n", (*stack)->n);
 }
-
 
 /**
 *popStack - remove the top element of the stack
@@ -67,4 +97,3 @@ void nopStack(stack_t **stack, unsigned int line_number);
     (void) stack;
     (void) line_number;
 }
-
