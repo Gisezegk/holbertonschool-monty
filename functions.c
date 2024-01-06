ebbe3cfae7a73669b0af258a445a6580472d2502
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-05 21:36:13 -0300

    functionsmonty

--- functions.c
+++ functions.c
@@ -7,13 +7,13 @@
 * @line_number: number of the line
 */
 
-void pallStack (stack_t **stack, unsigned int line_number)
+void pallStack(stack_t **stack, unsigned int line_number)
 {
     stack_t *tmp = *stack;
     tmp = *stack;
     (void)line_number;
 
-    while (tmp =! NULL)
+    while (tmp != NULL)
     {
         printf("%d\n", tmp->n);
 		tmp = tmp->next;
@@ -27,7 +27,7 @@ void pallStack (stack_t **stack, unsigned int line_number)
 *@stack: pointer to top the stack
 */
 
-void pushStack(stack_t **stack, unsigned int line_number);
+void pushStack(stack_t **stack, unsigned int line_number)
 {
     stack_t *nodo;
     char *j;
@@ -58,7 +58,7 @@ if (*stack != NULL)
 *@line_number: number of the line
 */
 
-void pintStack(stack_t **stack, unsigned int line_number);
+void pintStack(stack_t **stack, unsigned int line_number)
 {
     if (stack == NULL || *stack == NULL)
     {
@@ -74,7 +74,7 @@ void pintStack(stack_t **stack, unsigned int line_number);
 *@line_number: number of the line
 */
 
-void popStack(stack_t **stack, unsigned int line_number);
+void popStack(stack_t **stack, unsigned int line_number)
 {
     stack_t *j;
     if(stack == NULL || *stack == NULL)
@@ -92,7 +92,7 @@ void popStack(stack_t **stack, unsigned int line_number);
 *@line_number: number of the line
 */
 
-void nopStack(stack_t **stack, unsigned int line_number);
+void nopStack(stack_t **stack, unsigned int line_number)
 {
     (void) stack;
     (void) line_number;
