cc42afc31f3a8ae34b96b425cf914d1eea8bf133
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-05 22:08:40 -0300

    monty

--- monty.c
+++ monty.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
         opcode = get_op(charged_line, line_number);
         if (opcode == NULL)
                 continue;
-        result = execute(opcode, &stack, line_number);
+        result = execute(opcode, stack, line_number);
         if (result == -1)
                 exit(EXIT_FAILURE);
     }
