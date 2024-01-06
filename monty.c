b52b192e0a47a9058d7be56b60a1b75b4e731b87
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-05 23:30:18 -0300

    monty

--- monty.c
+++ monty.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     size_t len = 0;
     int read = 0, result = 0;
     unsigned int line_number = 0;
-    stack_t *stack = NULL;
+    stack_t **stack = NULL;
 
     if (argc != 2)
     {
