d3ae8e2714d3e794a06ffbd637ed44ab88fa466d
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-01-09 17:49:18 -0300

    functions

--- functions.c
+++ functions.c
@@ -74,7 +74,7 @@ void pushStack(stack_t **stack, unsigned int line_number)
 
 void pintStack(stack_t **stack, unsigned int line_number)
 {
-	if (stack == NULL || *stack == NULL)
+	if (!stack || !(*stack))
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
