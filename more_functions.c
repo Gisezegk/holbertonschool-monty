c898e65aa7db6d63b959016064a529e2eac67f91
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-01-09 13:39:06 +0000

    addfixing

--- more_functions.c
+++ more_functions.c
@@ -60,7 +60,7 @@ void addStack(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || !(*stack) || !(*stack)->next)
 	{
-		fprintf(stderr, "L<line_number>: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
